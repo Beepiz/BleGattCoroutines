6fef57b8dc132129d3e33287bec070e30534d9a8
Louis CAD <louis.cognault@gmail.com>
2017-12-27 14:08:00 +0100

    Add missing RequiresApi annotation

--- blegattcoroutines/src/main/java/com/beepiz/bluetooth/gattcoroutines/experimental/extensions/GattService.kt
+++ blegattcoroutines/src/main/java/com/beepiz/bluetooth/gattcoroutines/experimental/extensions/GattService.kt
@@ -1,9 +1,12 @@
 package com.beepiz.bluetooth.gattcoroutines.experimental.extensions
 
 import android.bluetooth.BluetoothGattService
+import android.os.Build.VERSION_CODES.JELLY_BEAN_MR2
+import android.support.annotation.RequiresApi
 import com.beepiz.bluetooth.gattcoroutines.experimental.BGC
 import java.util.*
 
+@RequiresApi(JELLY_BEAN_MR2)
 operator fun BluetoothGattService.get(characteristicUUID: UUID): BGC? {
     return getCharacteristic(characteristicUUID)
 }
