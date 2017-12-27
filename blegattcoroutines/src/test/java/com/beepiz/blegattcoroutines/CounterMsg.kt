538bc11075fdfeef4e998940334db34c19809b03
Louis CAD <louis.cognault@gmail.com>
2017-12-27 12:35:18 +0100

    Remove unused and non relevant tests

--- blegattcoroutines/src/test/java/com/beepiz/blegattcoroutines/CounterMsg.kt
+++
@@ -1,9 +0,0 @@
-package com.beepiz.blegattcoroutines
-
-import kotlinx.coroutines.experimental.CompletableDeferred
-
-/* Message types for counterActor */
-sealed class CounterMsg
-
-object IncCounter : CounterMsg() // one-way message to increment counter
-class GetCounter(val response: CompletableDeferred<Int>) : CounterMsg() // a request with reply
