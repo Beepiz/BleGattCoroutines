fd9c137da46117dd095e11da71b56654ed1c4f17
Louis CAD <louis.cognault@gmail.com>
2017-12-29 22:11:41 +0100

    Implement GenericAccess GATT Service

--- app/src/main/java/com/beepiz/blegattcoroutines/sample/MainActivity.kt
+++ app/src/main/java/com/beepiz/blegattcoroutines/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TODO()
---
+++ genericaccess/src/main/java/com/beepiz/blegattcoroutines/genericaccess/GenericAccess.kt
@@ -0,0 +1,41 @@
+package com.beepiz.blegattcoroutines.genericaccess
+
+import android.os.Build.VERSION_CODES.JELLY_BEAN_MR2
+import android.support.annotation.RequiresApi
+import com.beepiz.bluetooth.gattcoroutines.experimental.BGC
+import com.beepiz.bluetooth.gattcoroutines.experimental.GattConnection
+import com.beepiz.bluetooth.gattcoroutines.experimental.extensions.get
+import java.util.*
+import kotlin.experimental.and
+
+/**
+ * See [official docs here](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.generic_access.xml).
+ */
+@RequiresApi(JELLY_BEAN_MR2)
+object GenericAccess {
+    val uuid = UUID(0L, 0x1800)
+    val deviceNameUuid = UUID(0L, 0X2A00) // UTF-8
+    /** See constants in [Appearance]. */
+    val appearanceUuid = UUID(0L, 0x2A01) // 16 bit
+
+    suspend fun GattConnection.readDeviceName() = read(deviceNameUuid)
+    suspend fun GattConnection.readAppearance() = read(appearanceUuid)
+
+    val GattConnection.deviceName: String get() = get(deviceNameUuid).getStringValue(0)
+    val GattConnection.appearance: Short
+        get() = get(appearanceUuid).let {
+            (it.value[0].toInt() shl 8 or (it.value[1] and 0xFF.toByte()).toInt()).toShort()
+        }
+
+    private fun GattConnection.get(characteristicUuid: UUID): BGC {
+        val genericAccessService = getService(uuid) ?: throw IllegalStateException("Generic Access service not found. Make sure the service discovery has been performed!")
+        return genericAccessService[characteristicUuid] ?: throw IllegalStateException("Characteristic with UUID $characteristicUuid not found. Make sure the service discovery has been performed!")
+    }
+
+    private suspend fun GattConnection.read(characteristicUuid: UUID) {
+        readCharacteristic(get(characteristicUuid))
+    }
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun uuid(uuidString: String): UUID = UUID.fromString(uuidString)
+}
