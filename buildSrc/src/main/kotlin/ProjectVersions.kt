eb0eca667e9b696a73591b1120d21d9738ad39ae
Louis CAD <louis.cognault@gmail.com>
2019-05-08 12:29:52 +0200

    Prepare for release 0.4.1

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -1,7 +1,7 @@
 object ProjectVersions {
     const val androidBuildTools = "28.0.3"
     const val androidSdk = 28
-    const val thisLibrary = "0.5.0-dev-001"
+    const val thisLibrary = "0.4.1"
 }
 
 val isDevVersion = ProjectVersions.thisLibrary.contains("-dev-")
