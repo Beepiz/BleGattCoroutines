b3ffae3b14ee6df6f8efba3d6eab702e1347c5d1
Louis CAD <louis.cognault@gmail.com>
2022-02-11 14:39:08 +0100

    Add run-gradle-help workflow

---
+++ .github/workflows/run-gradle-help.main.kts
@@ -0,0 +1,24 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.6.1")
+
+import it.krzeminski.githubactions.actions.actions.CheckoutV2
+import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
+import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.writeToFile
+import java.nio.file.Paths
+
+val workflow = workflow(
+    name = "Test workflow",
+    on = listOf(Push()),
+    sourceFile = Paths.get(".github/workflows/run-gradle-help.main.kts"),
+    targetFile = Paths.get(".github/workflows/run-gradle-help.yml")
+) {
+    job(name = "test_job", runsOn = UbuntuLatest) {
+        uses(name = "Check out", action = CheckoutV2())
+        run(name = "Run gradle help", command = "./gradlew help")
+    }
+}
+
+workflow.writeToFile()
