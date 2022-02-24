f0719a7da52e60d90f7fb05de403d936e9b41193
Louis CAD <louis.cognault@gmail.com>
2022-02-24 12:05:06 +0100

    Set java source & compatibility compatibility to 1.8 in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,12 +8,13 @@ repositories {
     gradlePluginPortal()
 }
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(11))
-}
-
 dependencies {
     compileOnly(gradleApi())
     api(Android.tools.build.gradlePlugin)
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
+}
+
+tasks.compileJava {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
 }
