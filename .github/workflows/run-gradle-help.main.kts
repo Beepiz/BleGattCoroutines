d406ebfadc7f5337d583ed4395eb12f447aeceb5
Louis CAD <louis.cognault@gmail.com>
2022-02-24 12:38:42 +0100

    Run Test workflow only manually

--- .github/workflows/run-gradle-help.main.kts
+++ .github/workflows/run-gradle-help.main.kts
@@ -5,13 +5,14 @@
 import it.krzeminski.githubactions.actions.actions.CheckoutV2
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
 import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.domain.triggers.WorkflowDispatch
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
 import java.nio.file.Paths
 
 val workflow = workflow(
     name = "Test workflow",
-    on = listOf(Push()),
+    on = listOf(WorkflowDispatch()),
     sourceFile = Paths.get(".github/workflows/run-gradle-help.main.kts"),
     targetFile = Paths.get(".github/workflows/run-gradle-help.yml")
 ) {
